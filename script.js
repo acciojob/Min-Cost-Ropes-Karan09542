44fd52845d12e9143eb8bd57d1c68ae0a8264b50
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-08-28 19:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min 
-	arr.sort((a,b) => a-b)
-    if(arr.length === 1) return "Length must be > 2"
-    let prev = arr[0] + arr[1]
-    let count = prev;
-    for(let i = 2 ;i <arr.length; i++){
-        prev = prev + arr[i]
-        count += prev
+	let cost = 0;
+    while(arr.length >1){
+        arr.sort((a,b) => a-b)
+        const first = arr.shift()
+        const second = arr.shift()
+        const sum = first + second;
+        cost += sum
+        arr.push(sum)
     }
-    return count
+    return cost;
 }
 
 module.exports=mincost;
